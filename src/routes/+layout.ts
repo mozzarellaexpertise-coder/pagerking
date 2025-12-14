b7bdb20010dfcb036a61717b25f4ac0dc69783f3
GAT <mozzarellaexpertise@gmail.com>
2025-12-14 23:21:20 +0630

    :pppppp

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,6 @@
 // src/routes/+layout.ts
 
 import { getSupabaseClient, initializeSupabaseClient } from '$lib/supabaseClient';
-import { load as serverLoad } from './+layout.server.js';
 import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = async ({ data, fetch, depends }) => {
