743c7784856f40bf26d3f9c30357fd3e96504666
GAT <mozzarellaexpertise@gmail.com>
2025-12-19 23:46:55 +0630

    :SERVER GEM

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
-import adapterVercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto'; // NOT adapter-static
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapterVercel()
-    // no prerender block needed for SSR
-  }
+	preprocess: vitePreprocess(),
+	kit: {
+		// adapter-auto detects Vercel automatically and enables API routes
+		adapter: adapter()
+	}
 };
 
 export default config;
