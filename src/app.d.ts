3cc48c5875ce20565da6d502d8836d8605b04d75
GAT <mozzarellaexpertise@gmail.com>
2025-12-19 08:28:19 +0630

    :SHOOKSoo

---
+++ src/app.d.ts
@@ -0,0 +1,12 @@
+import type { SupabaseClient, User } from '@supabase/supabase-js';
+
+declare global {
+  namespace App {
+    interface Locals {
+      supabase: SupabaseClient;
+      user: User | null;
+    }
+  }
+}
+
+export {};
