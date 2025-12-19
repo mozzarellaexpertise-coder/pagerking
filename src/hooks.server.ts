372a1441de384fcf9b8899c6494004581b931f97
GAT <mozzarellaexpertise@gmail.com>
2025-12-19 17:30:58 +0630

    :My Server

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,10 +4,10 @@ import { createClient } from '@supabase/supabase-js';
 export const handle: Handle = async ({ event, resolve }) => {
   const supabaseAdmin = createClient(
     import.meta.env.SUPABASE_URL,
-    import.meta.env.SUPABASE_SERVICE_ROLE_KEY // MUST be service role key
+    import.meta.env.SUPABASE_SERVICE_ROLE_KEY
   );
 
-  // Get user from cookie (sent from browser)
+  // get user from cookie
   const {
     data: { user }
   } = await supabaseAdmin.auth.getUserByCookie(event.request);
