faeb8a7d32fcc937df6341e2fa1af84b5290c836
GAT <mozzarellaexpertise@gmail.com>
2025-12-20 00:49:52 +0630

    :rewrite

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
-import adapter from '@sveltejs/adapter-auto'; // NOT adapter-static
+import adapter from '@sveltejs/adapter-auto'; // or '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
+
 	kit: {
-		// adapter-auto detects Vercel automatically and enables API routes
-		adapter: adapter()
+		// This is the important part:
+		adapter: adapter({
+			runtime: 'nodejs22.x' 
+		})
 	}
 };
 
